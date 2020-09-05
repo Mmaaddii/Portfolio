5455d090cd26b95cc63c6bea1437f51715731bf6
Mmaaddii <66345451+Mmaaddii@users.noreply.github.com>
2020-09-05 17:49:54 +0200

    scroll top button

---
+++ javascript/scrollTop.js
@@ -0,0 +1,35 @@
+
+
+
+
+
+var button = document.getElementById("scrollButton");
+
+
+
+
+// Hide Show Scroll Top Button Function
+
+window.onscroll = function() {
+
+	// console.log(window.scrollY);
+
+	if (window.scrollY > 800) {
+		button.classList.remove("d-none");
+	}else {
+		button.classList.add("d-none");
+	}
+
+}
+
+// Scroll To Top Function
+
+function toTop() {
+
+		window.scroll({
+		  top: 0, 
+		  behavior: 'smooth'
+		});
+
+}
+
